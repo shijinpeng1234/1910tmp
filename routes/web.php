3df4654040a4b5cf6eded8620657269b90cbfc4b
sjp <1375221727@qq.com>
2020-06-21 18:26:50 +0800

    phpinfo

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 //测试一下
 Route::get('/test/hello','TestController@hello');
-Route::get('/info','TestController@info');
+Route::get('/info','TestController@info');//phpinfo
 
 //商品
 Route::get('/goods/detail','Goods\GoodsController@detail'); //商品详情
